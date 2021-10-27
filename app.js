94be9f58018f5557b9262a5f83fa877ce3106037
edw-nmv <eduard_naumov@yahoo.com>
2021-10-27 14:48:19 +0300

    init done

---
+++ app.js
@@ -0,0 +1,18 @@
+// Server config
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'hbs');
+app.set('view engine', path.join(__dirname, 'views'));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server started PORT: ${PORT}`);
+});
