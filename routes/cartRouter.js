2c96e46346e2dc3e2a00049fce1992e055c89c95
Alex_Kos <kosenkoaleksey34@gmail.com>
2021-10-28 17:13:51 +0300

    add_cart

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const { Sock } = require('../db/models');
+const { User, Sock } = require('../db/models');
 
 router.get('/', async (req, res) => {
+  const email = req.session.user.email;
   
-  let isUser;
-  let goodsList;
-  
+  let user;
+  let socksList;
 
   try {
-    goodsList = await Sock.findAll()
+    user = await User.findOne({ where: { email }, raw: true })
   }
   catch (error) {
     return res
@@ -19,10 +19,28 @@ router.get('/', async (req, res) => {
         path: '/cart'
       })
   }
-  if (!goodsList.length > 0) {
+  try {
+    socksList = await Sock.findAll({ where: { UserId: user.id } })
+  }
+  catch (error) {
+    return res
+      .status(500)
+      .json({
+        registration: false,
+        message: 'Что-то пошло не так, попробуйте перезагрузить страницу',
+        path: '/cart'
+      })
+  }
+  if (socksList.length <= 0) {
     return res.render('cart', { empty: true })
   }
-  res.render('cart', { goodsList, empty: false })
+  res.render('cart', { socksList, empty: false })
+})
+
+router.get('/delete/:id', async (req, res) => {
+  const id = +req.params.id
+  const isDel = await Sock.destroy({ where: { id } })
+  res.redirect('/cart')
 })
 
 module.exports = router;
