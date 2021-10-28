7892819b6868637de55afaf2608ec661904c318b
Kate <letterforkatherine@gmail.com>
2021-10-28 19:53:24 +0300

    check

--- routes/generateRouter.js
+++ routes/generateRouter.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
-const { Sock } = require('../db/models');
+// const { Sock } = require('../db/models');
 
 router.get('/', (req, res) => {
   res.render('generate-socks');
 });
 
-router.get('/', async (req, res) => {
-  const id = +req.params.id;
-  // const sock = await Sock.findOne({ where: { id: req.params.id } });
-  const updateSock = await Sock.update({ isFavourite: true }, {
-    where: { id: req.params.id },
-  })
-  res.json();
-})
+// router.get('/', async (req, res) => {
+//   const id = +req.params.id;
+//   // const sock = await Sock.findOne({ where: { id: req.params.id } });
+//   const updateSock = await Sock.update({ isFavourite: true }, {
+//     where: { id: req.params.id },
+//   })
+//   res.json();
+// })
 
 module.exports = router;
